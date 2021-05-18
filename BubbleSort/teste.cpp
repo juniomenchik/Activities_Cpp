2afe032330cb53712776f19aa3bde41c2c112ebe
Jeferson Junio Menchik <juniomenchik@live.com>
2021-05-18 00:37:03 -0300

    init VSCODE
    Para compilar C++ e  C  F8 !! !

---
+++ BubbleSort/teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // f8 para compilar em um console
+    printf("Chesque do bresque ");
+
+    return 0;
+}
